b5a046a7d41101bb99f2ee9dfe31f15331907bd6
Jeremy Walker <jeremy@meducation.net>
2013-04-10 17:10:34 +0100

    Install sphinx from source

--- cookbooks/dependencies/recipes/default.rb
+++ cookbooks/dependencies/recipes/default.rb
@@ -1,8 +1,28 @@
-%w{sphinx mysql mysql-devel git patch gcc gcc-devel make zlib-devel curl-devel openssl openssl-devel gcc gcc-c++ kernel-devel libxml2-devel ImageMagick ImageMagick-devel libxslt libxslt-devel memcached readline-devel modssl}.each do |dependency|
-  package dependency
+# Install via package manager
+#============================
+%w{mysql mysql-devel git patch gcc gcc-devel make zlib-devel curl-devel openssl openssl-devel gcc gcc-c++ kernel-devel libxml2-devel ImageMagick ImageMagick-devel libxslt libxslt-devel memcached readline-devel modssl}.each do |pkg|
+  package pkg
 end
 
-#service "sphinx" do
-#  supports status: true, restart: true, reload: true
-#  action [:enable, :start]
-#end
+# Install from source
+#====================
+{
+  "sphinx" => "http://sphinxsearch.com/files/sphinx-2.0.7-release.tar.gz"
+}.each do |program, url|
+  filename = url.split("/").last
+  directory = filename.gsub(".tar.gz", "")
+  remote_file "/ec2-user/downloads/#{filename}.tar.gz" do
+    source url
+    notifies :run, "bash[install_#{program}]", :immediately
+  end
+
+  bash "install_#{program}" do
+    user "root"
+    cwd "/tmp"
+    code <<-EOH
+      tar -zxf #{filename}
+      (cd #{directory}/ && ./configure && make && make install)
+    EOH
+    action :nothing
+  end
+end
