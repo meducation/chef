c4d68db6af0270c09e2858b2b7bdedf3f1dbd368
Jeremy Walker <jeremy@meducation.net>
2013-04-10 17:12:10 +0100

    Remove gcc-devel

--- cookbooks/dependencies/recipes/default.rb
+++ cookbooks/dependencies/recipes/default.rb
@@ -1,6 +1,6 @@
 # Install via package manager
 #============================
-%w{mysql mysql-devel git patch gcc gcc-devel make zlib-devel curl-devel openssl openssl-devel gcc gcc-c++ kernel-devel libxml2-devel ImageMagick ImageMagick-devel libxslt libxslt-devel memcached readline-devel modssl}.each do |pkg|
+%w{mysql mysql-devel git patch gcc make zlib-devel curl-devel openssl openssl-devel gcc-c++ kernel-devel libxml2-devel ImageMagick ImageMagick-devel libxslt libxslt-devel memcached readline-devel modssl}.each do |pkg|
   package pkg
 end
 
