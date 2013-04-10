ae0e01a0f07bb5061cc770b4b7168b3499e8b5d7
Jeremy Walker <jeremy@meducation.net>
2013-04-10 14:11:20 +0100

    Fix broken website recipe

--- cookbooks/meducation-website/recipes/default.rb
+++ cookbooks/meducation-website/recipes/default.rb
@@ -3,18 +3,46 @@ include_recipe "unicorn"
 
 gem_package "bundler"
 
-common = {name: "meducation-website", app_root: "u/apps/meducation/website"}
+common = {name: "meducation-website", app_root: "/srv/apps/meducation-website"}
 
 directory common[:app_root] do
-  owner "ec2-user"
-end
-
-directory "#{common[:app_root]}/current" do
-  owner "ec2-user"
-end
-
-%w(config log tmp socket pids).each do |dir|
-  directory "#{common[:app_root]}/shared/#{dir}"
   recursive true
-  mode 0775
+  owner "ec2-user"
+end
+
+directory "#{common[:app_root]}/releases" do
+  owner "ec2-user"
+  group "ec2-user"
+end
+
+%w(log tmp sockets pids cache attachments).each do |dir|
+  directory "#{common[:app_root]}/shared/#{dir}" do
+    owner "ec2-user"
+    group "ec2-user"
+    recursive true
+    mode 0775
+  end
+end
+
+template "#{node[:unicorn][:config_path]}/#{common[:name]}.conf.rb" do
+  mode 0644
+  source "unicorn.conf.erb"
+  variables common
+end
+
+nginx_config_path = "/etc/nginx/sites-available/#{common[:name]}.conf"
+
+template nginx_config_path do
+  mode 0644
+  source "nginx.conf.erb"
+  variables common.merge(
+    server_name: "www.meducation.net"
+    redirect_server_names: "meducation.net"
+  )
+  notifies :reload, "service[nginx]"
+end
+
+nginx_site common[:name] do
+  config_path nginx_config_path
+  action :enable
 end
